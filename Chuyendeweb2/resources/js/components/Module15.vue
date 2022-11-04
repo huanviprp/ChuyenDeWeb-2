0454436799ad8eb86e670297b05f9f792412568f
AnPhan <tlt2542001@gmail.com>
2022-11-04 17:27:24 +0700

    update module15

--- Chuyendeweb2/resources/js/components/Module15.vue
+++ Chuyendeweb2/resources/js/components/Module15.vue
@@ -1,146 +1,371 @@
 <template>
-   <div class="admin-panel clearfix">
-  <div class="slidebar">
-    <div class="logo">
-      <a href=""></a>
+    <div class="admin-panel clearfix">
+        <div class="slidebar">
+            <div class="logo">
+                <a href=""></a>
+            </div>
+            <ul>
+                <li><a href="#dashboard" id="targeted">dashboard</a></li>
+                <li><a href="#user">User</a></li>
+                <li><a href="#comic">Comic</a></li>
+                <li><a href="#category">Thể loại</a></li>
+                <li><a href="#chapter">Chapter</a></li>
+                <li><a href="#author">Tác giả</a></li>
+                <li><a href="#comment">Bình luận</a></li>
+            </ul>
+        </div>
+        <div class="main">
+            <div class="mainContent clearfix">
+                <div id="dashboard">
+                    <div class="topmain" style="display: flex; align-items: center; justify-content: space-between;">
+                        <h2 class="header"><span class="icon"></span>Dashboard</h2>
+                        <a href="/" title="Trang chủ"> <i class="fa-solid fa-house"></i></a>
+                    </div>
+                    <div class="monitor">
+                        <div class="clearfix">
+                            <ul class="content">
+                                <li>User</li>
+                                <li class="posts"><span class="count">179</span><a href="">posts</a></li>                               
+                            </ul>
+                            <ul class="discussions">
+                                <li>Comic</li>
+                                <li class="comments"><span class="count">353</span><a href="">comments</a></li>
+                                <li class="approved"><span class="count">319</span><a href="">approved</a></li>
+                                <li class="pending"><span class="count">0</span><a href="">pending</a></li>
+                                <li class="spam"><span class="count">34</span><a href="">spam</a></li>
+                            </ul>
+                        </div>
+                        <p>Theme <a href="">Twenty Eleven</a> with <a href="">3 widgets</a></p>
+                    </div>
+                </div>
+                <div id="user">
+                    <div class="topmain" style="display: flex; align-items: center; justify-content: space-between;">
+                        <h2 class="header"><span class="icon"></span>User</h2>
+                        <a href="/" title="Trang chủ"> <i class="fa-solid fa-house"></i></a>
+                    </div>
+                    <div class="monitor">
+                        <div class="clearfix">
+                            <ul class="content">
+                                <li>content</li>
+                                <li class="posts"><span class="count">179</span><a href="">posts</a></li>
+                                <li class="pages"><span class="count">13</span><a href="">pages</a></li>
+                                <li class="categories"><span class="count">21</span><a href="">categories</a></li>
+                                <li class="tags"><span class="count">305</span><a href="">tags</a></li>
+                            </ul>
+                        </div>
+                        <p>Theme <a href="">Twenty Eleven</a> with <a href="">3 widgets</a></p>
+                    </div>
+                </div>
+                <div id="comic">
+                    <div class="topmain" style="display: flex; align-items: center; justify-content: space-between;">
+                        <h2 class="header"><span class="icon"></span>Comic</h2>
+                        <a href="/" title="Trang chủ"> <i class="fa-solid fa-house"></i></a>
+                    </div>
+                    <div class="monitor">
+                        <div class="clearfix">
+                            <ul class="content">
+                                <li>content</li>
+                                <li class="posts"><span class="count">179</span><a href="">posts</a></li>
+                                <li class="pages"><span class="count">13</span><a href="">pages</a></li>
+                                <li class="categories"><span class="count">21</span><a href="">categories</a></li>
+                                <li class="tags"><span class="count">305</span><a href="">tags</a></li>
+                            </ul>
+                            <ul class="discussions">
+                                <li>discussions</li>
+                                <li class="comments"><span class="count">353</span><a href="">comments</a></li>
+                                <li class="approved"><span class="count">319</span><a href="">approved</a></li>
+                                <li class="pending"><span class="count">0</span><a href="">pending</a></li>
+                                <li class="spam"><span class="count">34</span><a href="">spam</a></li>
+                            </ul>
+                        </div>
+                        <p>Theme <a href="">Twenty Eleven</a> with <a href="">3 widgets</a></p>
+                    </div>
+                </div>
+                <div id="category">
+                    <div class="topmain" style="display: flex; align-items: center; justify-content: space-between;">
+                        <h2 class="header"><span class="icon"></span>category</h2>
+                        <a href="/" title="Trang chủ"> <i class="fa-solid fa-house"></i></a>
+                    </div>
+                    <div class="monitor">
+                        <div class="clearfix">
+                            <ul class="content">
+                                <li>content</li>
+                                <li class="posts"><span class="count">179</span><a href="">posts</a></li>
+                                <li class="pages"><span class="count">13</span><a href="">pages</a></li>
+                                <li class="categories"><span class="count">21</span><a href="">categories</a></li>
+                                <li class="tags"><span class="count">305</span><a href="">tags</a></li>
+                            </ul>
+                            <ul class="discussions">
+                                <li>discussions</li>
+                                <li class="comments"><span class="count">353</span><a href="">comments</a></li>
+                                <li class="approved"><span class="count">319</span><a href="">approved</a></li>
+                                <li class="pending"><span class="count">0</span><a href="">pending</a></li>
+                                <li class="spam"><span class="count">34</span><a href="">spam</a></li>
+                            </ul>
+                        </div>
+                        <p>Theme <a href="">Twenty Eleven</a> with <a href="">3 widgets</a></p>
+                    </div>
+                </div>
+                <div id="chapter">
+                    <div class="topmain" style="display: flex; align-items: center; justify-content: space-between;">
+                        <h2 class="header"><span class="icon"></span>chapter</h2>
+                        <a href="/" title="Trang chủ"> <i class="fa-solid fa-house"></i></a>
+                    </div>
+                    <div class="monitor">
+                        <div class="clearfix">
+                            <ul class="content">
+                                <li>content</li>
+                                <li class="posts"><span class="count">179</span><a href="">posts</a></li>
+                                <li class="pages"><span class="count">13</span><a href="">pages</a></li>
+                                <li class="categories"><span class="count">21</span><a href="">categories</a></li>
+                                <li class="tags"><span class="count">305</span><a href="">tags</a></li>
+                            </ul>
+                            <ul class="discussions">
+                                <li>discussions</li>
+                                <li class="comments"><span class="count">353</span><a href="">comments</a></li>
+                                <li class="approved"><span class="count">319</span><a href="">approved</a></li>
+                                <li class="pending"><span class="count">0</span><a href="">pending</a></li>
+                                <li class="spam"><span class="count">34</span><a href="">spam</a></li>
+                            </ul>
+                        </div>
+                        <p>Theme <a href="">Twenty Eleven</a> with <a href="">3 widgets</a></p>
+                    </div>
+                </div>
+                <div id="author">
+                    <div class="topmain" style="display: flex; align-items: center; justify-content: space-between;">
+                        <h2 class="header"><span class="icon"></span>author</h2>
+                        <a href="/" title="Trang chủ"> <i class="fa-solid fa-house"></i></a>
+                    </div>
+                    <div class="monitor">
+                        <div class="clearfix">
+                            <ul class="content">
+                                <li>content</li>
+                                <li class="posts"><span class="count">179</span><a href="">posts</a></li>
+                                <li class="pages"><span class="count">13</span><a href="">pages</a></li>
+                                <li class="categories"><span class="count">21</span><a href="">categories</a></li>
+                                <li class="tags"><span class="count">305</span><a href="">tags</a></li>
+                            </ul>
+                            <ul class="discussions">
+                                <li>discussions</li>
+                                <li class="comments"><span class="count">353</span><a href="">comments</a></li>
+                                <li class="approved"><span class="count">319</span><a href="">approved</a></li>
+                                <li class="pending"><span class="count">0</span><a href="">pending</a></li>
+                                <li class="spam"><span class="count">34</span><a href="">spam</a></li>
+                            </ul>
+                        </div>
+                        <p>Theme <a href="">Twenty Eleven</a> with <a href="">3 widgets</a></p>
+                    </div>
+                </div>
+                <div id="comment">
+                    <div class="topmain" style="display: flex; align-items: center; justify-content: space-between;">
+                        <h2 class="header"><span class="icon"></span>comment</h2>
+                        <a href="/" title="Trang chủ"> <i class="fa-solid fa-house"></i></a>
+                    </div>
+                    <div class="monitor">
+                        <div class="clearfix">
+                            <ul class="content">
+                                <li>content</li>
+                                <li class="posts"><span class="count">179</span><a href="">posts</a></li>
+                                <li class="pages"><span class="count">13</span><a href="">pages</a></li>
+                                <li class="categories"><span class="count">21</span><a href="">categories</a></li>
+                                <li class="tags"><span class="count">305</span><a href="">tags</a></li>
+                            </ul>
+                            <ul class="discussions">
+                                <li>discussions</li>
+                                <li class="comments"><span class="count">353</span><a href="">comments</a></li>
+                                <li class="approved"><span class="count">319</span><a href="">approved</a></li>
+                                <li class="pending"><span class="count">0</span><a href="">pending</a></li>
+                                <li class="spam"><span class="count">34</span><a href="">spam</a></li>
+                            </ul>
+                        </div>
+                        <p>Theme <a href="">Twenty Eleven</a> with <a href="">3 widgets</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <ul>
-      <li><a href="#dashboard" id="targeted">dashboard</a></li>
-      <li><a href="#posts">posts</a></li>
-    </ul>
-  </div>
-  <div class="main">
-    <div class="mainContent clearfix">
-      <div id="dashboard">
-        <h2 class="header"><span class="icon"></span>Dashboard</h2> 
-       </div>
-       <div id="posts">
-         <h2 class="header">Posts</h2>
-       </div>
-     </div>
-   </div>
-</div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
 <style>
 body {
-	background-color: rgb(220,220,220);
-	padding: 0;
-	margin: 0;
-	font-size: 16px;
-	font-family: sans-serif;
+    background-image: url('https://cdn.wallpapersafari.com/17/62/Mw5A6y.jpg');
+    padding: 0;
+    margin: 0;
+    font-size: 16px;
+    font-family: sans-serif;
 }
 
 .admin-panel {
-	width: 990px;
-	margin: 50px auto;
+    width: 90%;
+    margin: 20px auto;
 }
+
 /*slidebar*/
 .slidebar {
-	width: 20%;
-	min-height: 690px;
-	float: left;
-	border-right: 1px solid rgb(235,235,235);
-	background-color: rgb(247,247,247);
+    width: 20%;
+    min-height: 690px;
+    float: left;
+    border-right: 1px solid rgb(235, 235, 235);
+    background-color: rgb(247, 247, 247);
 }
+
 .slidebar .logo {
-	height: 145px;
-	border-bottom: 1px solid rgb(235,235,235);
+    height: 145px;
+    border-bottom: 1px solid rgb(235, 235, 235);
 }
+
 .slidebar ul {
-	padding: 0;
-	margin:0;
+    padding: 0;
+    margin: 0;
 }
+
 .slidebar li {
-	list-style-type: none;
-	margin: 0;
-	position: relative;
+    list-style-type: none;
+    margin: 0;
+    position: relative;
 }
 
 .slidebar ul a {
-	color: rgb(140,140,140);
-	text-decoration: none;
-	font:16px/40px helvetica,verdana,sans-serif;
-	box-sizing:border-box;
-	border-bottom: 1px solid rgb(235,235,235);
-	display: block;
-	box-shadow:inset 0 1px 0 rgb(255,255,255);
-	text-indent: 20px;
-	text-transform: capitalize;
+    color: rgb(140, 140, 140);
+    text-decoration: none;
+    font: 16px/40px helvetica, verdana, sans-serif;
+    box-sizing: border-box;
+    border-bottom: 1px solid rgb(235, 235, 235);
+    display: block;
+    box-shadow: inset 0 1px 0 rgb(255, 255, 255);
+    text-indent: 20px;
+    text-transform: capitalize;
 }
+
 .slidebar li:hover a {
-	background-color: rgb(255,255,255);
-	box-shadow: 1px 0 0 rgb(255,255,255),inset 5px 0 0 -1px rgb(234,83,63);
+    background-color: rgb(255, 255, 255);
+    box-shadow: 1px 0 0 rgb(255, 255, 255), inset 5px 0 0 -1px rgb(234, 83, 63);
 }
+
 /*main*/
 .main {
-	float: left;
-	width: 79%;
-	height: 690px;
-	background-color: rgb(255,255,255);
-	position: relative;
-	font-family: helvetica,verdana,sans-serif;
+    float: left;
+    width: 79%;
+    height: 690px;
+    background-color: rgb(255, 255, 255);
+    position: relative;
+    font-family: helvetica, verdana, sans-serif;
 }
+
 .main .topbar {
-	border-bottom: 1px solid rgb(235,235,235);
-	margin: 0;
-	padding: 0;
+    border-bottom: 1px solid rgb(235, 235, 235);
+    margin: 0;
+    padding: 0;
 }
 
 /*mainContent*/
- 
+
 .mainContent>div {
-	position: absolute;
-	opacity: 0;
-	transition:opacity 200ms linear;
-}
-/*通过opacity来切换不同的选项卡*/
-.mainContent>div:target {
-	opacity: 1;
-}
-.mainContent h2 {
-	margin:1em 30px;
-	color: rgb(234,83,63);
-	font-size: 20px;
+    width: 100%;
+
+    position: absolute;
+    opacity: 0;
+    border: 1px solid rgb(235, 235, 235);
+    transition: opacity 200ms linear;
 }
 
- #dashboard>div {
-	border: 1px solid rgb(235,235,235);
-	margin-left: 30px;
-	border-radius: 5px;
-	min-width: 345px;
-	height: 262px;
-	display: inline-block;
+.mainContent>div i {
+    margin-right: 30px;
+}
+
+/*通过opacity来切换不同的选项卡*/
+.mainContent>div:target {
+    opacity: 1;
+}
+
+.mainContent h2 {
+    margin: 1em 30px;
+    color: rgb(234, 83, 63);
+    font-size: 20px;
+    text-transform: uppercase;
+    font-weight: 700;
+}
+
+.topmain{
+    background-color: #F7F7F7;
+    border: 1px solid rgb(235, 235, 235);
 }
 /*logo*/
 .logo a {
-	width: 88px;
-	height: 88px;
-	display: inline-block;
-	position: relative;
-	left: 50%;
-	top: 50%;
-	margin: -45px 0 0 -45px;
-	border: 1px solid rgb(200,200,200);
-	border-radius: 50%;
-	background-color: rgb(214,214,214);
+    width: 88px;
+    height: 88px;
+    display: inline-block;
+    position: relative;
+    left: 50%;
+    top: 50%;
+    margin: -45px 0 0 -45px;
+    border: 1px solid rgb(200, 200, 200);
+    border-radius: 50%;
+    background-color: rgb(214, 214, 214);
 }
+
 .logo a:before {
-	content: "A";
-	width: 70px;
-	height: 70px;
-	font: 50px/70px helvetica,verdana,sans-serif;
-	text-align: center;
-	position: absolute;
-	top: 8px;
-	left: 8px;
-	border-radius: 35px;
-	border: 1px solid rgb(210,210,210);
+    content: "A";
+    width: 70px;
+    height: 70px;
+    font: 50px/70px helvetica, verdana, sans-serif;
+    text-align: center;
+    position: absolute;
+    top: 8px;
+    left: 8px;
+    border-radius: 35px;
+    border: 1px solid rgb(210, 210, 210);
+    display: inline-block;
+    background: linear-gradient(top, rgb(255, 255, 255), rgb(245, 245, 245));
+}
+.monitor{
+    margin-left: 30px;
+    margin-top: 30px;
+}
+.monitor ul {
+	float: left;
+	padding: 0;
+	margin: 0 31px 0 17px;
+}
+.monitor li {
+	list-style:none;
+	font: 600 14px/28px helvetica,verdana,sans-serif;
+	color: rgb(102,102,102);
+	text-transform: capitalize;
+}
+.monitor li a {
+	color: rgb(102,102,102);
+	text-transform: capitalize;
+	text-decoration: none;
+}
+.monitor li:first-child {
+	border-bottom: 1px dotted rgb(153,153,153);
+}
+.discussions .comments {color: rgb(27,106,173);}
+.discussions .approved {color: rgb(105,174,48);}
+.discussions .pending {color: rgb(246,129,15);}
+.discussions .spam {color: rgb(243,47,47);}
+.monitor .count {
+	color: rgb(27,106,173);
+	text-align: right;
+	font-weight: 600;
+	margin-right: 15px;
+	min-width: 25px;
 	display: inline-block;
-	background: linear-gradient(top,rgb(255,255,255),rgb(245,245,245));
+}
+.monitor p {
+	color: rgb(128,128,128);
+	margin: 28px 0 18px 17px;
+	display: block;
+	font-weight: 600;
+	font-size: 12px;
+}
+.monitor p a {
+	text-decoration: none;
+	color:rgb(27,106,173);
 }
 </style>
