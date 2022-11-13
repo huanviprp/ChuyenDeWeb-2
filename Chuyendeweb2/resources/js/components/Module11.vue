dcd8416cf9ac49de766e3e02c02817b659a15e3c
nguyenquocit <93866373+nguyenquocit@users.noreply.github.com>
2022-11-13 15:26:58 +0700

    commit update main

--- Chuyendeweb2/resources/js/components/Module11.vue
+++ Chuyendeweb2/resources/js/components/Module11.vue
@@ -1,43 +1,43 @@
 <template>
     <div class="reading-comic text-center">
         <div id="page_0" class="page-chapter">
-            <img src="https://truyenvua.xyz/3755/539/0.jpg?gf=hdfgdfg"
+            <img src="//cdn.ntcdntempv3.com/data/images/20265/925928/001-403102c.jpg?data=net"
                 data-original="https://truyenvua.xyz/3755/539/0.jpg?gf=hdfgdfg" alt="Siêu nhân di?n quang">
         </div>
         <div id="page_1" class="page-chapter">
-            <img src="https://truyenvua.xyz/3755/539/1.jpg?gf=hdfgdfg"
+            <img src="//cdn.ntcdntempv3.com/data/images/20265/925928/002-5ccea08.jpg?data=net"
                 data-original="https://truyenvua.xyz/3755/539/1.jpg?gf=hdfgdfg" alt="Siêu nhân di?n quang">
         </div>
         <div id="page_2" class="page-chapter">
-            <img src="https://truyenvua.xyz/3755/539/2.jpg?gf=hdfgdfg"
+            <img src="//cdn.ntcdntempv3.com/data/images/20265/925928/003-1ae90e1.jpg?data=net"
                 data-original="https://truyenvua.xyz/3755/539/2.jpg?gf=hdfgdfg" alt="Siêu nhân di?n quang">
         </div>
         <div id="page_3" class="page-chapter">
-            <img src="https://truyenvua.xyz/3755/539/3.jpg?gf=hdfgdfg"
+            <img src="//cdn.ntcdntempv3.com/data/images/20265/925928/004-71a046a.jpg?data=net"
                 data-original="https://truyenvua.xyz/3755/539/3.jpg?gf=hdfgdfg" alt="Siêu nhân di?n quang">
         </div>
         <div id="page_4" class="page-chapter">
-            <img src="https://truyenvua.xyz/3755/539/4.jpg?gf=hdfgdfg"
+            <img src="//cdn.ntcdntempv3.com/data/images/20265/925928/005-d38c8bd.jpg?data=net"
                 data-original="https://truyenvua.xyz/3755/539/4.jpg?gf=hdfgdfg" alt="Siêu nhân di?n quang">
         </div>
         <div id="page_5" class="page-chapter">
-            <img src="https://truyenvua.xyz/3755/539/5.jpg?gf=hdfgdfg"
+            <img src="//cdn.ntcdntempv3.com/data/images/20265/925928/006-74ffcd5.jpg?data=net"
                 data-original="https://truyenvua.xyz/3755/539/5.jpg?gf=hdfgdfg" alt="Siêu nhân di?n quang">
         </div>
         <div id="page_6" class="page-chapter">
-            <img src="https://truyenvua.xyz/3755/539/6.jpg?gf=hdfgdfg"
+            <img src="//cdn.ntcdntempv3.com/data/images/20265/925928/007-50daff0.jpg?data=net"
                 data-original="https://truyenvua.xyz/3755/539/6.jpg?gf=hdfgdfg" alt="Siêu nhân di?n quang">
         </div>
         <div id="page_7" class="page-chapter">
-            <img src="https://truyenvua.xyz/3755/539/7.jpg?gf=hdfgdfg"
+            <img src="//cdn.ntcdntempv3.com/data/images/20265/925928/008-bb00265.jpg?data=net"
                 data-original="https://truyenvua.xyz/3755/539/7.jpg?gf=hdfgdfg" alt="Siêu nhân di?n quang">
         </div>
         <div id="page_8" class="page-chapter">
-            <img src="https://truyenvua.xyz/3755/539/8.jpg?gf=hdfgdfg"
+            <img src="//cdn.ntcdntempv3.com/data/images/20265/925928/009-f4d7a59.jpg?data=net"
                 data-original="https://truyenvua.xyz/3755/539/8.jpg?gf=hdfgdfg" alt="Siêu nhân di?n quang">
         </div>
         <div id="page_9" class="page-chapter">
-            <img src="https://truyenvua.xyz/3755/539/9.jpg?gf=hdfgdfg"
+            <img src="//cdn.ntcdntempv3.com/data/images/20265/925928/010-1fa754d.jpg?data=net"
                 data-original="https://truyenvua.xyz/3755/539/9.jpg?gf=hdfgdfg" alt="Siêu nhân di?n quang">
         </div>
     </div>
