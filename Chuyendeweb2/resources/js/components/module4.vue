22d7c2bd3f9a0bf91266868d2dcef4c894a3cc87
nguyenquocit <93866373+nguyenquocit@users.noreply.github.com>
2022-11-18 15:38:57 +0700

    data home

--- Chuyendeweb2/resources/js/components/module4.vue
+++ Chuyendeweb2/resources/js/components/module4.vue
@@ -3,9 +3,9 @@
         <p style="color:#de6e98;padding-top:0.5cm"><b>Truyện New Update <i class="fa fa-angle-right"
                     aria-hidden="true"></i></b></p>
     </h3>
-    <div class="row comic-new">
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
+    <div  class="row comic-new" v-if="Comics && Comics.length">
+        <div class="col-md-3 comic" v-for="Comic of Comics" :key="Comic.Comic_id">
+            <a href="/detail"><img class="img-comic" v-bind:src="Comic.Img_feature"></a>
             <div class=" infor-comic">
                 <ul>
                     <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
@@ -14,7 +14,7 @@
                 </ul>
             </div>
             <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
+                <a class="name-comic" v-bind:href="'/detail/'+ Comic.Comic_id"><b>{{Comic.Name}}</b></a>
                 <ul>
                     <li><a href="">Chapter 3</a></li>
                     <li><a href="">Chapter 2</a></li>
@@ -22,265 +22,24 @@
                 </ul>
             </div>
         </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 comic">
-            <a href="/detail"><img class="img-comic" src="https://tse3.mm.bing.net/th?id=OIP.oJhAtynqCdcsrNOO0RAWYgHaMW&pid=Api&P=0"></a>
-            <div class=" infor-comic">
-                <ul>
-                    <li><i class="fa fa-eye" aria-hidden="true"></i>123</li>
-                    <li><i class="fa fa-comment" aria-hidden="true"></i> 123</li>
-                    <li><i class="fa fa-heart" aria-hidden="true"></i> 123</li>
-                </ul>
-            </div>
-            <div class="new-chapter">
-                <a class="name-comic" href="/detail"><b>Tên truyện</b></a>
-                <ul>
-                    <li><a href="">Chapter 3</a></li>
-                    <li><a href="">Chapter 2</a></li>
-                    <li><a href="">Chapter 1</a></li>
-                </ul>
-            </div>
-        </div>
-        
     </div>
 </template>
   
 <script>
+import axios from 'axios';
 export default {
-
+    data() {
+       return{
+        Comics:[]
+       }
+    },
+    created() {
+        axios.get('api/').then(
+            res =>{
+                this.Comics = res.data;
+            }
+        )
+    },
 }
 </script>
   
