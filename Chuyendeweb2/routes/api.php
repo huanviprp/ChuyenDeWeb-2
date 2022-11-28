67e4bf32e9b97569a678aeec7799be2086e2c8b9
AnPhan <tlt2542001@gmail.com>
2022-11-28 13:10:00 +0700

    update

--- Chuyendeweb2/routes/api.php
+++ Chuyendeweb2/routes/api.php
@@ -17,13 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-Route::post('login', [UserController::class, 'login']);
-Route::post('register', [UserController::class, 'register']);
-Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+// Route::post('/login', [UserController::class, 'login']);
+// Route::post('register', [UserController::class, 'register']);
+// Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
 
 
 Route::post('/dataComic', [ComicController::class, 'index']);
-Route::get('/datauser', [UserController::class, 'index']);
+Route::get('/datauser', [DangkyController::class, 'index']);
