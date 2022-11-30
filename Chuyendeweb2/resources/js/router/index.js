829928576fa9a216caa114cf9796eb19c7f3aad5
cucdaucho@gmail.com <cucdaucho@gmail.com>
2022-11-30 10:27:28 +0700

    create

---
+++ Chuyendeweb2/resources/js/router/index.js
@@ -0,0 +1,50 @@
+// import Module10 from './components/Module10.vue';
+// import Module11 from './components/Module11.vue';
+// import Module12 from './components/Module12.vue';
+// import Module13 from './components/Module13.vue';
+// import Module14 from './components/Module14.vue';
+// import Module15 from './components/Module15.vue';
+// import Module1 from './components/Module1.vue';
+// import Module2 from './components/Module2.vue';
+// import Module3 from './components/Module3.vue';
+// import Module4 from './components/Module4.vue';
+// import Module5 from './components/Module5.vue';
+// import Module6 from './components/Module6.vue';
+// import Module7 from './components/Module7.vue';
+// import Module8 from './components/Module8.vue';
+// import Module9 from './components/Module9.vue';
+// import Breadcrumb from './components/Breadcrumb.vue';
+// import Module16 from './components/module16.vue';
+// import Module17 from './components/module17.vue';
+// import Module18 from './components/module18.vue';
+
+
+export const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'register',
+    path: '/register/:id',
+    component: Module14
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: Module13
+  },
+  {
+    name: 'dashboard',
+    path: '/dashboard',
+    component: Module15
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
+
+export default router;
