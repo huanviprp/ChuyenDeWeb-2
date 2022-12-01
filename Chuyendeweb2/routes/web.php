7704db277c1ec0aabd98f03bef43893eb0c5b94a
cucdaucho@gmail.com <cucdaucho@gmail.com>
2022-12-01 13:56:45 +0700

    change

--- Chuyendeweb2/routes/web.php
+++ Chuyendeweb2/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('home');
-// });
+Route::get('/', function () {
+    return view('home');
+});
 
 // Route::get('/module12', function () {
 //     return view('module12');
@@ -58,6 +58,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('home');
 // });
-Route::get('{any}', function () {
-    return view('app');
-})->where('any', '.*');
+// Route::get('{any}', function () {
+//     return view('app');
+// })->where('any', '.*');
